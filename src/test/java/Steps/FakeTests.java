daee3766e0dd1fe1c7d1fa2eb824c2ffcfe39636
Milena Drabik <midr@softwaremind.com>
2018-11-21 10:05:01 +0100

    expanded fake tests

--- src/test/java/Steps/FakeTests.java
+++ src/test/java/Steps/FakeTests.java
@@ -1,10 +1,19 @@
 package Steps;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 import org.testng.annotations.Test;
+
+@Epic("All framework testing test")
+@Feature("Szkolenie Appium")
 
 public class FakeTests {
 
     @Test(groups = {"smoke","regression"})
+    @Story("This is fake test")
+    @Description("This test will never fail")
     public void fakeSmokeTest() {
     }
 
